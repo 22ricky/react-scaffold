31be587ed822f4e9102027e253304e03988b321c
22ricky <wyprivatemail@163.com>
2019-03-27 10:54:57 +0800

    编译图片

--- src/pages/Page1/Page1.js
+++ src/pages/Page1/Page1.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './Page1.css';
+import image from './images/horse.png';
 
 export default class Page1 extends Component {
   render() {
     return (
       <div className="page-box">
         This is Page1~
+        <img src={image} />
       </div>
     );
   }
