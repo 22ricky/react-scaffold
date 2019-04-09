e9425eff8c453d6644e4e1220712e159dd23bbfc
22ricky <wyprivatemail@163.com>
2019-04-09 13:40:09 +0800

    使用 CSS Modules

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ A React Scaffold
 
 - uglifyjs-webpack-plugin 0.4.6
 - clean-webpack-plugin 0.1.16
+- css-loader 0.28.7
--- src/pages/Page1/Page1.js
+++ src/pages/Page1/Page1.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './Page1.css';
+import style from './Page1.css';
 import image from './images/horse.png';
 
 export default class Page1 extends Component {
   render() {
     return (
-      <div className="page-box">
+      <div className={style.box}>
         This is Page1~
         <img src={image} />
       </div>
